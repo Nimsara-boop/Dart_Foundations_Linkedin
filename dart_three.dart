1f2d2ee9ebcfe3c5ac7ea6e0514a88dffbd9dfc7
Pugsy <nimsarakarunaratne@gmail.com>
2025-07-29 00:27:03 +0530

    What is Type Inferencing and Annotation. Learned those 2 terms. Presidence of Logical and arithmetic operations

---
+++ dart_three.dart
@@ -0,0 +1,24 @@
+//Type Inference and Type Annotations
+
+//type inference is the compiler automotically determining the data type of a varibale. (ex. var)
+//Type Annotation is specifying the data type of a variable or expression
+
+
+//Logical Operators and Arithmetic Operators
+
+void main(){
+
+var num1 = 10;
+var num2 = 5;
+var result = num1 < num2 || ++num1 <num2;
+
+print(result);
+
+var num3 = 10;
+var num4 = 5;
+var result2 = num3 + num4 *2 -3 ; 
+
+print(result2);
+
+
+}
